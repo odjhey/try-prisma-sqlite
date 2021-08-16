fa470ebfcbfd6067f48fa907d9328f9465c22c8b
Odelon Jhey Pacalso <odjhey@gmail.com>
2021-08-16 12:36:49 +0800

    add mutations

--- backend/src/schema.js
+++ backend/src/schema.js
@@ -5,10 +5,31 @@ const typeDefs = gql`
     id: Int!
     name: String!
     description: String
+    tasks: [Task!]!
+  }
+
+  type Task {
+    id: Int!
+    description: String
+    todo: Todo
   }
 
   type Query {
     allTodos: [Todo!]!
+  }
+
+  type Mutation {
+    createTodo(data: TodoCreateInput!): Todo
+  }
+
+  input TodoCreateInput {
+    name: String!
+    description: String
+    tasks: [TaskCreateInput!]!
+  }
+
+  input TaskCreateInput {
+    description: String!
   }
 `;
 
@@ -16,6 +37,40 @@ const resolvers = {
   Query: {
     allTodos: (_p, _a, context) => {
       return context.prisma.todo.findMany();
+    },
+  },
+  Todo: {
+    tasks: (parent, _args, ctx) => {
+      return ctx.prisma.todo.findUnique({ where: { id: parent.id } }).tasks();
+    },
+  },
+  Task: {
+    todo: (parent, _args, ctx) => {
+      return ctx.prisma.task
+        .findUnique({
+          where: { id: parent.id },
+        })
+        .todo();
+    },
+  },
+  Mutation: {
+    createTodo: (_parent, args, context) => {
+      const tasks = args.data.tasks
+        ? args.data.tasks.map((task) => {
+            return {
+              description: task.description,
+            };
+          })
+        : [];
+      return context.prisma.todo.create({
+        data: {
+          name: args.data.name,
+          description: args.data.description,
+          tasks: {
+            create: tasks,
+          },
+        },
+      });
     },
   },
 };
